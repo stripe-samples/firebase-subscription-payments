817e1eaf043a02fc641fa0fe5377fba48001de2f
Thorsten Schaeff <thorsten@stripe.com>
2020-07-10 16:34:56 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,58 @@
+# Run Subscription Payments with the Stripe Firebase Extension
+
+This is an example web client for usage with the [Stripe Subscriptions Firebase Extension](https://firebase.google.com/products/extensions/firestore-stripe-subscriptions).
+
+**Features:**
+
+- Renders product and pricing details from your Cloud Firestore.
+- Creates Stripe Checkout Sessions to start subscriptions for your customers.
+- Syncs your subscription statuses with Firebase Authentication for access control management.
+- Gives your users access to the Stripe Customer Portal.
+
+## Demo
+
+- https://stripe-subs-ext.web.app/
+
+The design for Stripe Checkout and the customer portal can be customized in your Stripe Dashboard [branding settings](https://dashboard.stripe.com/settings/branding). See this example which is customized to match the Firebase color scheme:
+
+![Stripe Checkout Page](https://storage.googleapis.com/stripe-subscriptions-firebase-screenshots/firebase-stripe-subs-checkout.png)
+![Stripe Customer Portal](https://storage.googleapis.com/stripe-subscriptions-firebase-screenshots/firebase-stripe-subs-customer-portal.png)
+
+## Setup
+
+If you haven't already, [install the Firebase CLI](https://firebase.google.com/docs/cli):
+
+```bash
+npm install -g firebase-tools
+```
+
+### Install and configure the Firebase Extension
+
+1. Install the [Stripe Subscriptions Firebase Extension](https://firebase.google.com/products/extensions/firestore-stripe-subscriptions).
+2. Configure the extension in your Firebase Console.
+
+### Configure the example web client
+
+- In the [/public/javascript/app.js](/public/javascript/app.js) file set your
+  - `STRIPE_PUBLISHABLE_KEY` from the [Stripe Dashboard](https://dashboard.stripe.com/apikeys).
+  - `firebaseConfig` object from your Firebase console project settings.
+
+## Run locally
+
+```bash
+firebase serve
+# or
+npm run dev
+```
+
+## Deploy to Firebase Hosting
+
+```bash
+firebase deploy
+# or
+npm run deploy
+```
+
+## Author
+
+- [@thorsten-stripe](https://twitter.com/thorwebdev)
---
+++ public/javascript/app.js
@@ -0,0 +1,200 @@
+// Replace with your publishable key
+// https://dashboard.stripe.com/apikeys
+const STRIPE_PUBLISHABLE_KEY = 'pk_test_NzVWw6MB7fN3HSeAvVnyf5tx00hTu3Ukrk';
+
+// Replace with your Firebase project config.
+const firebaseConfig = {
+  apiKey: 'AIzaSyAEGmffBNUsVrdVS_iyiI4eUMOWWp4Q5dI',
+  authDomain: 'stripe-subs-ext.firebaseapp.com',
+  databaseURL: 'https://stripe-subs-ext.firebaseio.com',
+  projectId: 'stripe-subs-ext',
+  storageBucket: 'stripe-subs-ext.appspot.com',
+  messagingSenderId: '955066520266',
+  appId: '1:955066520266:web:ec7135a76fea7a1bce9a33',
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+/**
+ * Firebase Authentication configuration
+ */
+const firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
+const firebaseUiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: () => {
+      document.querySelector('#loader').style.display = 'none';
+    },
+  },
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  // Your terms of service url.
+  tosUrl: 'https://example.com/terms',
+  // Your privacy policy url.
+  privacyPolicyUrl: 'https://example.com/privacy',
+};
+firebase.auth().onAuthStateChanged((firebaseUser) => {
+  if (firebaseUser) {
+    document.querySelector('#loader').style.display = 'none';
+    document.querySelector('main').style.display = 'block';
+    currentUser = firebaseUser.uid;
+    startDataListeners();
+  } else {
+    document.querySelector('main').style.display = 'none';
+    firebaseUI.start('#firebaseui-auth-container', firebaseUiConfig);
+  }
+});
+
+/**
+ * Data listeners
+ */
+function startDataListeners() {
+  // Get all our products and render them to the page
+  const products = document.querySelector('.products');
+  const template = document.querySelector('#product');
+  db.collection('products')
+    .where('active', '==', true)
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(async function (doc) {
+        const priceSnap = await doc.ref
+          .collection('prices')
+          .orderBy('unit_amount')
+          .get();
+        if (!'content' in document.createElement('template')) {
+          console.error('Your browser doesn’t support HTML template elements.');
+          return;
+        }
+
+        const product = doc.data();
+        const container = template.content.cloneNode(true);
+
+        container.querySelector('h2').innerText = product.name.toUpperCase();
+        container.querySelector('.description').innerText =
+          product.description.toUpperCase() || '';
+        // Prices dropdown
+        priceSnap.docs.forEach((doc) => {
+          const priceId = doc.id;
+          const priceData = doc.data();
+          const content = document.createTextNode(
+            `${new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: priceData.currency,
+            }).format((priceData.unit_amount / 100).toFixed(2))} per ${
+              priceData.interval
+            }`
+          );
+          const option = document.createElement('option');
+          option.value = priceId;
+          option.appendChild(content);
+          container.querySelector('#price').appendChild(option);
+        });
+
+        if (product.images.length) {
+          const img = container.querySelector('img');
+          img.src = product.images[0];
+          img.alt = product.name;
+        }
+
+        const form = container.querySelector('form');
+        form.addEventListener('submit', subscribe);
+
+        products.appendChild(container);
+      });
+    });
+  // Get all subscriptions for the customer
+  db.collection('customers')
+    .doc(currentUser)
+    .collection('subscriptions')
+    .where('status', '==', 'active')
+    .onSnapshot(async (snapshot) => {
+      if (snapshot.empty) {
+        // Show products
+        document.querySelector('#subscribe').style.display = 'block';
+        return;
+      }
+      document.querySelector('#subscribe').style.display = 'none';
+      document.querySelector('#my-subscription').style.display = 'block';
+      // In this implementation we only expect one Subscription to exist
+      const subscription = snapshot.docs[0].data();
+      const priceData = (await subscription.price.get()).data();
+      document.querySelector(
+        '#my-subscription p'
+      ).textContent = `You are paying ${new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: priceData.currency,
+      }).format((priceData.unit_amount / 100).toFixed(2))} per ${
+        priceData.interval
+      }, giving you the role: ${await getCustomClaimRole()}. 🥳`;
+    });
+}
+
+/**
+ * Event listeners
+ */
+
+// Signout button
+document
+  .getElementById('signout')
+  .addEventListener('click', () => firebase.auth().signOut());
+
+// Checkout handler
+async function subscribe(event) {
+  event.preventDefault();
+  event.target.querySelector('button').disabled = true;
+  const formData = new FormData(event.target);
+
+  const docRef = await db
+    .collection('customers')
+    .doc(currentUser)
+    .collection('checkout_sessions')
+    .add({
+      price: formData.get('price'),
+      success_url: window.location.origin,
+      cancel_url: window.location.origin,
+    });
+  // Wait for the CheckoutSession to get attached by the extension
+  docRef.onSnapshot((snap) => {
+    const { sessionId } = snap.data();
+    if (sessionId) {
+      // We have a session, let's redirect to Checkout
+      // Init Stripe
+      const stripe = Stripe(STRIPE_PUBLISHABLE_KEY);
+      stripe.redirectToCheckout({ sessionId });
+    }
+  });
+}
+
+// Billing portal handler
+document
+  .querySelector('#billing-portal-button')
+  .addEventListener('click', async (event) => {
+    event.target.disabled = true;
+
+    // Call billing portal function
+    const functionRef = firebase
+      .app()
+      .functions('us-east1')
+      .httpsCallable('ext-firestore-stripe-subscriptions-createPortalLink');
+    const { data } = await functionRef({ returnUrl: window.location.origin });
+    window.location.assign(data.url);
+  });
+
+// Get custom claim role helper
+async function getCustomClaimRole() {
+  await firebase.auth().currentUser.getIdToken(true);
+  const decodedToken = await firebase.auth().currentUser.getIdTokenResult();
+  return decodedToken.claims.stripeRole;
+}
