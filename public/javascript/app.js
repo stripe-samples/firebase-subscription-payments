4b2a2de96520d335637a9e8cbd3e20b5d95e388b
Thorsten Schaeff <thorsten@stripe.com>
2020-08-14 16:02:46 +0800

    Codelab start.

---
+++ public/javascript/app.js
@@ -0,0 +1,73 @@
+/**
+ * Replace with your publishable key from the Stripe Dashboard
+ * https://dashboard.stripe.com/apikeys
+ */
+const STRIPE_PUBLISHABLE_KEY = 'TODO';
+
+/**
+ * Your Firebase config from the Firebase console
+ * https://firebase.google.com/docs/web/setup#config-object
+ */
+const firebaseConfig = {
+  apiKey: 'TODO',
+  authDomain: 'TODO',
+  databaseURL: 'TODO',
+  projectId: 'TODO',
+  storageBucket: 'TODO',
+  messagingSenderId: 'TODO',
+  appId: 'TODO',
+};
+
+/**
+ * Initialize Firebase
+ */
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+/**
+ * Firebase Authentication configuration
+ */
+const firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
+const firebaseUiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: () => {
+      document.querySelector('#loader').style.display = 'none';
+    },
+  },
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  // Your terms of service url.
+  tosUrl: 'https://example.com/terms',
+  // Your privacy policy url.
+  privacyPolicyUrl: 'https://example.com/privacy',
+};
+firebase.auth().onAuthStateChanged((firebaseUser) => {
+  if (firebaseUser) {
+    document.querySelector('#loader').style.display = 'none';
+    document.querySelector('main').style.display = 'block';
+    currentUser = firebaseUser.uid;
+    startDataListeners();
+  } else {
+    document.querySelector('main').style.display = 'none';
+    firebaseUI.start('#firebaseui-auth-container', firebaseUiConfig);
+  }
+});
+
+/**
+ * Data listeners
+ */
+
+/**
+ * Event listeners
+ */
